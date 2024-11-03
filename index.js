a84011f1ffc875b2b6004a6da3d888f583523c98
Iqbal AD <iqbalandhikad@student.telkomuniversity.ac.id>
2024-11-03 22:01:52 +0700

    test
    awalan

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const setUp = express()
+const port = 3523
+
+setUp.get('/', (req, res) => {
+    res.send('Welcome to LMS')
+})
+
+setUp.listen(port, () => {
+    // console.log('Server berjalan pada http://localhost',port)
+    console.log(`Server berjalan pada http://localhost:${port}`)
+} )
